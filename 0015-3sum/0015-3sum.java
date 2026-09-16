ee973baf270c1287c6a99320a423ff1d5b35ef64
Yogesh Raghuvanshi <yogeshraghuvanshi613@gmail.com>
2026-09-16 20:38:50 +0530

    Time: 32 ms (76.17%), Space: 59 MB (74.55%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -20,16 +20,16 @@ class Solution {
                 if (sum == 0) {
                     ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                    while (left < right && nums[left] == nums[left + 1]) {
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) {
                         left++;
                     }
 
-                    while (left < right && nums[right] == nums[right - 1]) {
+                    while (left < right && nums[right] == nums[right + 1]) {
                         right--;
                     }
-
-                    left++;
-                    right--;
                 } else if (sum < 0) {
                     left++;
                 } else {
